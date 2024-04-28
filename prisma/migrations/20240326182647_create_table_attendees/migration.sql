d0419ae9e3beecebc2448a261b0f0000375cf7d3
debora berte <arobedbee@gmail.com>
2024-04-27 23:32:14 -0300

    chore: adjust mutations from sqlite pattern to postgresql

--- prisma/migrations/20240326182647_create_table_attendees/migration.sql
+++ prisma/migrations/20240326182647_create_table_attendees/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "attendees" (
-    "id" serial INTEGER NOT NULL PRIMARY KEY,
+    "id" serial NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
--- prisma/migrations/20240327143815_add_cascades/migration.sql
+++ prisma/migrations/20240327143815_add_cascades/migration.sql
@@ -1,26 +1,45 @@
--- RedefineTables
-PRAGMA foreign_keys=OFF;
-CREATE TABLE "new_check_ins" (
-    "id" serial INTEGER NOT NULL PRIMARY KEY,
+BEGIN;
+
+-- Create a new temporary table with desired structure
+CREATE TABLE new_check_ins (
+    "id" SERIAL PRIMARY KEY,
     "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "attendee_id" INTEGER NOT NULL,
-    CONSTRAINT "check_ins_attendee_id_fkey" FOREIGN KEY ("attendee_id") REFERENCES "attendees" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "new_check_ins_attendee_id_fkey" FOREIGN KEY ("attendee_id") REFERENCES "attendees" ("id") ON DELETE CASCADE
 );
-INSERT INTO "new_check_ins" ("attendee_id", "created_at", "id") SELECT "attendee_id", "created_at", "id" FROM "check_ins";
-DROP TABLE "check_ins";
-ALTER TABLE "new_check_ins" RENAME TO "check_ins";
-CREATE UNIQUE INDEX "check_ins_attendee_id_key" ON "check_ins"("attendee_id");
-CREATE TABLE "new_attendees" (
-    "id" serial INTEGER NOT NULL PRIMARY KEY,
+
+-- Savepoint for partial rollback in case of errors
+SAVEPOINT before_insert_check_ins;
+
+-- Copy data from old table to new table
+INSERT INTO new_check_ins ("attendee_id", "created_at", "id")
+SELECT "attendee_id", "created_at", "id" FROM check_ins;
+
+-- If there are no errors, drop the old table and rename the new one
+DROP TABLE check_ins;
+ALTER TABLE new_check_ins RENAME TO check_ins;
+
+-- Repeat process for attendees
+SAVEPOINT before_create_attendees;
+CREATE TABLE new_attendees (
+    "id" SERIAL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "event_id" TEXT NOT NULL,
-    CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "new_attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE CASCADE
 );
-INSERT INTO "new_attendees" ("created_at", "email", "event_id", "id", "name") SELECT "created_at", "email", "event_id", "id", "name" FROM "attendees";
-DROP TABLE "attendees";
-ALTER TABLE "new_attendees" RENAME TO "attendees";
+
+SAVEPOINT before_insert_attendees;
+INSERT INTO new_attendees ("created_at", "email", "event_id", "id", "name")
+SELECT "created_at", "email", "event_id", "id", "name" FROM attendees;
+
+DROP TABLE attendees;
+ALTER TABLE new_attendees RENAME TO attendees;
+
+-- Create any necessary indexes
+CREATE UNIQUE INDEX "check_ins_attendee_id_key" ON "check_ins"("attendee_id");
 CREATE UNIQUE INDEX "attendees_event_id_email_key" ON "attendees"("event_id", "email");
-PRAGMA foreign_key_check;
-PRAGMA foreign_keys=ON;
+
+-- Commit all changes
+COMMIT;
